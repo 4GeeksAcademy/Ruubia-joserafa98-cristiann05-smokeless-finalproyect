f4c6d518c30f44c1197c5c86b974f81bd8e1833b
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-09-27 19:47:00 +0000

    add class coach

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,34 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Coach(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email_coach = db.Column(db.String(120), unique=True, nullable=False)
+    password_coach = db.Column(db.String(128), nullable=False)
+    nombre_coach = db.Column(db.String(50), nullable=False)
+    genero_coach = db.Column(db.String(10), nullable=False)
+    direccion = db.Column(db.String(200))
+    latitud = db.Column(db.Float)
+    longitud = db.Column(db.Float)
+    descripcion_coach = db.Column(db.Text)
+    foto_coach = db.Column(db.String(200))
+    precio_servicio = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Coach {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email_coach": self.email_coach,
+            "nombre_coach": self.nombre_coach,
+            "genero_coach": self.genero_coach,
+            "direccion": self.direccion,
+            "latitud": self.latitud,
+            "longitud": self.longitud,
+            "descripcion_coach": self.descripcion_coach,
+            "foto_coach": self.foto_coach,
+            "precio_servicio": self.precio_servicio
+            # do not serialize the password, its a security breach
+        }
