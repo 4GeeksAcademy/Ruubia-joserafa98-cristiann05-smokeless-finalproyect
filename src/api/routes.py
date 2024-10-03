7ca3266620ea321a3d450ed96c80bc4c72bd3fde
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-10-03 12:09:08 +0000

    update routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, SmokerUser, Coach, TiposConsumo, Seguimiento, Mensajes
+from api.models import db, SmokerUser, Coach, TiposConsumo, Seguimiento, Mensajes, Solicitud
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from datetime import datetime
