01f6e1eb392a3948e3d21bef740a1d02d53b2de8
Cristian Ayala Sánchez <ayalasanchezcristian2000@gmail.com>
2024-10-09 00:00:48 +0000

    filtrado por ciudad perfecto

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,7 +4,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/homePage";
 import SignupSmoker from "./pages/signupSmoker";
-import LoginSmoker from "./pages/loginSmoker"; 
+import LoginSmoker from "./pages/loginSmoker";
 import ControlPanelSmoker from "./pages/controlPanelSmoker";
 import SignupCoach from "./pages/signupCoach";
 import LoginCoach from "./pages/loginCoach";
@@ -21,9 +21,9 @@ import ViewProfileCoach from "./pages/ViewProfileCoach";
 import UserProfile from "./pages/userProfile";
 import CoachAddress from "./pages/CoachAddress";
 
-import CoachCard from "./component/CoachCards";
+import CoachesList from "./pages/CoachesList";
 import ApprovedCoaches from "./pages/coachapprove";
-import Navbar from "./component/navbar"; 
+import Navbar from "./component/navbar";
 import Footer from "./component/footer";
 import CoachProfile from "./pages/CoachProfile";
 
@@ -57,11 +57,11 @@ const Layout = () => {
                 <ScrollToTop>
                     {/* Renderiza el Navbar solo si la ruta no está en hiddenRoutes */}
                     {!hiddenRoutes.includes(window.location.pathname) && (
-                        <Navbar 
-                            toggleTheme={toggleTheme} 
-                            language={language} 
-                            handleLanguageChange={handleLanguageChange} 
-                            theme={theme} 
+                        <Navbar
+                            toggleTheme={toggleTheme}
+                            language={language}
+                            handleLanguageChange={handleLanguageChange}
+                            theme={theme}
                         />
                     )}
                     <Routes>
@@ -82,7 +82,7 @@ const Layout = () => {
                         <Route element={<ViewProfileCoach />} path="/coach-details</:coachId>" />
                         <Route element={<UserProfile />} path="/user-profile/:userId" />
                         <Route element={<CoachAddress />} path="/question-address-coach" />
-                        <Route element={<CoachCard />} path="/coach-available" /> 
+                        <Route element={<CoachesList />} path="/coaches" />
                         <Route element={<ApprovedCoaches />} path="/approved-coaches" />
                         <Route element={<CoachProfile />} path="/coach-profile/:coachId" />
                         <Route element={<h1>Not found!</h1>} />
